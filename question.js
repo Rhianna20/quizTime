1f8b4a9c31f962d1da988baefa34f226a6a26e1b
Rhianna Stuart <rhiannastuart@hotmail.co.uk>
2020-06-05 13:53:32 +0100

    Its Demo timee!

--- question.js
+++ question.js
@@ -1,8 +1,8 @@
 const startButton = document.getElementById('startbtn')
 const questionCElement = document.getElementById('container-question')
 const questionBlock = document.getElementById('question');
-var answerBlock = document.getElementById('answwer-btn');
-var nextButton = document.getElementById('next-btn');
+const answerBlock = document.getElementById('answer-btn');
+const nextButton = document.getElementById('nextbtn');
 
 let shuffleQuestions, questionIndex;
 
@@ -11,31 +11,70 @@ startButton.addEventListener('click', startQuiz);
 function startQuiz(){
     console.log('The quiz has begun!')
     startButton.classList.add('hide')
+    nextButton.classList.add('hide')
     shuffleQuestions = questions.sort(() => Math.random() - .2)
     questionIndex = 0
     questionCElement.classList.remove('hide')
     nextUp()
 }
+answerBlock.addEventListener('click', () => {
+    questionIndex
+})
 
-function nextUp(){
-    revealQuestion(shuffleQuestions[questionIndex])     
+function restart(){
+    nextButton.classList.add('hide')
+    while (answerBlock.firstChild){
+        answerBlock.removeChild(answerBlock.firstChild)
+    }
+}
+
+
+function revealQuestion(question){
+    questionBlock.innerHTML = question.question
+    question.answer.forEach(answer => {
+        let button = document.createElement('button')
+        button.innerHTML = answer.text
+        button.classList.add('btn')
+        if (answer.correct){
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerBlock.appendChild(button)
+    }
+        
+    );
     }
 
 
 
+function nextUp(){
+    restart() 
+    revealQuestion(shuffleQuestions[questionIndex])   
+    }
 
-function revealQuestion(question){
-questionBlock.innerHTML = question.question
-question.answer.forEach(answer => {
-    const button = document.createElement('button')
-    button.innerHTML = answer.text
-    button.classList.add('btn')
-}
-    
-);
-}
-function selectAnswer(){
 
+function cleanUp(element,correct){
+    setOff(element)
+    if (correct){
+        element.classList.add('correct')
+    }
+    else { element.classList.add('wrong')}
+}
+
+function clearUp(){
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
+
+function selectAnswer(event){
+const buttonSelected = event.target
+const correct = buttonSelected.dataset.correct
+restart(document.body, correct)
+Array.from(answerBlock.children).forEach(button => {   
+restart(button, button.dataset.correct)
+})
+if (shuffleQuestions.length > questionIndex + 1)
+nextButton.classList.remove('hide')
 
 }
  
@@ -48,18 +87,18 @@ const questions = [
         {text: 'd', correct: false}
     ]},  
         {question: 'What does HTML stand for', answer: [
-        {text: 'a', correct: true},
-        {text: 'b', correct: false},
+        {text: 'a', correct: false},
+        {text: 'b', correct: true},
         {text: 'c', correct: false},
         {text: 'd', correct: false}
     ]},  
-    {question: 'What is Javascript usejd for?', answer: [
-        {text: 'a', correct: true},
+    {question: 'What is CSS?', answer: [
+        {text: 'a', correct: false},
         {text: 'b', correct: false},
         {text: 'c', correct: false},
-        {text: 'd', correct: false}
+        {text: 'd', correct: true}
     ]},  
-    {question: 'What is Javajkkscript used for?', answer: [
+    {question: 'What SCRUM?', answer: [
         {text: 'a', correct: true},
         {text: 'b', correct: false},
         {text: 'c', correct: false},
