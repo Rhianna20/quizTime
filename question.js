e47564b626ca972b1d587d1b814396dd5153ee29
Rhianna Stuart <rhiannastuart@hotmail.co.uk>
2020-06-07 02:32:58 +0100

    ...

--- question.js
+++ question.js
@@ -3,10 +3,15 @@ const questionCElement = document.getElementById('container-question')
 const questionBlock = document.getElementById('question');
 const answerBlock = document.getElementById('answer-btn');
 const nextButton = document.getElementById('nextbtn');
-
+const button = document.getElementById('button')
+const answerbut = document.getElementById('answerbuttons')
+const answer = document.getElementById('answer')
 let shuffleQuestions, questionIndex;
 
 startButton.addEventListener('click', startQuiz);
+
+
+
 
 function startQuiz(){
     console.log('The quiz has begun!')
@@ -20,6 +25,18 @@ function startQuiz(){
 answerBlock.addEventListener('click', () => {
     questionIndex
 })
+answerBlock.addEventListener('click', revealQuestion);
+nextButton.addEventListener('click',() => {
+    questionIndex++
+    nextUp()
+})
+
+
+function nextUp(){
+    restart() 
+    revealQuestion(shuffleQuestions[questionIndex])  
+    
+    }
 
 function restart(){
     nextButton.classList.add('hide')
@@ -31,15 +48,18 @@ function restart(){
 
 function revealQuestion(question){
     questionBlock.innerHTML = question.question
-    question.answer.forEach(answer => {
-        let button = document.createElement('button')
+    Array.from(question.answer).forEach(answer => {
+        const button = document.createElement('button')
         button.innerHTML = answer.text
         button.classList.add('btn')
         if (answer.correct){
             button.dataset.correct = answer.correct
+        
         }
         button.addEventListener('click', selectAnswer)
         answerBlock.appendChild(button)
+        
+
     }
         
     );
@@ -47,37 +67,45 @@ function revealQuestion(question){
 
 
 
-function nextUp(){
-    restart() 
-    revealQuestion(shuffleQuestions[questionIndex])   
-    }
-
-
-function cleanUp(element,correct){
-    setOff(element)
-    if (correct){
-        element.classList.add('correct')
-    }
-    else { element.classList.add('wrong')}
+function selectAnswer(event){
+const buttonSelected = event.target
+const correct = buttonSelected.dataset.correct
+resetit(document.body, correct)
+Array.from(answerBlock.children).forEach(button => {   
+resetit(button, button.dataset.correct)
+})
+if (shuffleQuestions.length > questionIndex + 1)
+nextButton.classList.remove('hide')
+console.log('first question')
+answer.addEventListener('click', grandrev)
 }
 
-function clearUp(){
+function granrev(){
+    if(answer === true){
+        console.log('ding ding!')
+    }
+    else
+    console.log('woopp')
+}
+
+function resetit (element, correct){
+    clearup(element)
+    if (correct){
+        element.classList.add('correct')
+        console.log('correct')
+    }
+    else {
+        element.classList.add('wrong')
+        console.log('wrong')
+    }
+}
+
+
+function clearup(element){
     element.classList.remove('correct')
     element.classList.remove('wrong')
 }
 
-function selectAnswer(event){
-const buttonSelected = event.target
-const correct = buttonSelected.dataset.correct
-restart(document.body, correct)
-Array.from(answerBlock.children).forEach(button => {   
-restart(button, button.dataset.correct)
-})
-if (shuffleQuestions.length > questionIndex + 1)
-nextButton.classList.remove('hide')
-
-}
- 
 
 const questions = [
     {question: 'What is Javascript used for?', answer: [
@@ -87,27 +115,23 @@ const questions = [
         {text: 'd', correct: false}
     ]},  
         {question: 'What does HTML stand for', answer: [
-        {text: 'a', correct: false},
+        {text: 'A', correct: false},
         {text: 'b', correct: true},
-        {text: 'c', correct: false},
+        {text: 'S', correct: false},
         {text: 'd', correct: false}
     ]},  
     {question: 'What is CSS?', answer: [
         {text: 'a', correct: false},
-        {text: 'b', correct: false},
+        {text: 'bDDD', correct: false},
         {text: 'c', correct: false},
-        {text: 'd', correct: true}
+        {text: 'dDDDD', correct: true}
     ]},  
     {question: 'What SCRUM?', answer: [
-        {text: 'a', correct: true},
+        {text: 'aDDD', correct: true},
         {text: 'b', correct: false},
-        {text: 'c', correct: false},
+        {text: 'cDDDD', correct: false},
         {text: 'd', correct: false}
-    ]},  
-
-
+    ]}
         ]
     
-
-
 
