86e245f288a621e0d16407f7a2c3aa37aa5f819b
Rhianna Stuart <rhiannastuart@hotmail.co.uk>
2020-06-08 15:58:52 +0100

    not long left :)

--- question.js
+++ question.js
@@ -4,8 +4,8 @@ const questionBlock = document.getElementById('question');
 const answerBlock = document.getElementById('answer-btn');
 const nextButton = document.getElementById('nextbtn');
 const button = document.getElementById('button')
-const answerbut = document.getElementById('answerbuttons')
 const answer = document.getElementById('answer')
+let score = 0;
 let shuffleQuestions, questionIndex;
 
 startButton.addEventListener('click', startQuiz);
@@ -13,7 +13,8 @@ startButton.addEventListener('click', startQuiz);
 
 
 
-function startQuiz(){
+function startQuiz()
+  {
     console.log('The quiz has begun!')
     startButton.classList.add('hide')
     nextButton.classList.add('hide')
@@ -21,49 +22,48 @@ function startQuiz(){
     questionIndex = 0
     questionCElement.classList.remove('hide')
     nextUp()
-}
-answerBlock.addEventListener('click', () => {
-    questionIndex
+ }
+    answerBlock.addEventListener('click', () => {
+    question.innerHTML
 })
-answerBlock.addEventListener('click', revealQuestion);
-nextButton.addEventListener('click',() => {
+    answerBlock.addEventListener('click', selectAnswer);
+    nextButton.addEventListener('click',() => {
     questionIndex++
     nextUp()
 })
 
 
-function nextUp(){
+  function nextUp()
+{
     restart() 
     revealQuestion(shuffleQuestions[questionIndex])  
-    
-    }
+    questionCElement.classList.remove('hide')
+}
 
-function restart(){
+function restart()
+ {  
+    clearup(document.body)
     nextButton.classList.add('hide')
     while (answerBlock.firstChild){
         answerBlock.removeChild(answerBlock.firstChild)
-    }
+   }
 }
 
 
 function revealQuestion(question){
     questionBlock.innerHTML = question.question
-    Array.from(question.answer).forEach(answer => {
+    question.answer.forEach(answer => {
         const button = document.createElement('button')
         button.innerHTML = answer.text
         button.classList.add('btn')
         if (answer.correct){
             button.dataset.correct = answer.correct
         
-        }
+ }
         button.addEventListener('click', selectAnswer)
         answerBlock.appendChild(button)
-        
-
-    }
-        
-    );
-    }
+    }     );
+}
 
 
 
@@ -76,27 +76,21 @@ resetit(button, button.dataset.correct)
 })
 if (shuffleQuestions.length > questionIndex + 1)
 nextButton.classList.remove('hide')
-console.log('first question')
-answer.addEventListener('click', grandrev)
+console.log('Answer Selected')
+
 }
 
-function granrev(){
-    if(answer === true){
-        console.log('ding ding!')
-    }
-    else
-    console.log('woopp')
-}
+
 
 function resetit (element, correct){
     clearup(element)
     if (correct){
+
         element.classList.add('correct')
-        console.log('correct')
+      
     }
-    else {
+    else if (answerBlock != true) {
         element.classList.add('wrong')
-        console.log('wrong')
     }
 }
 
@@ -106,32 +100,42 @@ function clearup(element){
     element.classList.remove('wrong')
 }
 
+function endOfQuiz(shuffleQuestions){
+    if (shuffleQuestions < questionIndex){
+        console.log('End of quiz! Woorayy!')
+        nextButton.classList.remove('hide')
+        questionBlock.innerHTML = "End of Quiz!"
+    } else if (buttonSelected) {
+        console.log('error')
+    }
+}
 
-const questions = [
+
+var questions = [
     {question: 'What is Javascript used for?', answer: [
-        {text: 'a', correct: true},
-        {text: 'b', correct: false},
-        {text: 'c', correct: false},
-        {text: 'd', correct: false}
+        {text: 'To style Web Page', correct: false},
+        {text: 'frontend development framework for creting responsive web design', correct: false},
+        {text: 'Creates interactive effects within web browser', correct: true},
+        {text: 'A shape produced by a curve', correct: false}
     ]},  
         {question: 'What does HTML stand for', answer: [
-        {text: 'A', correct: false},
-        {text: 'b', correct: true},
-        {text: 'S', correct: false},
+        {text: 'HyperTxt Makeup Language', correct: false},
+        {text: 'Hypertext Markup Language', correct: true},
+        {text: 'hypertext', correct: false},
         {text: 'd', correct: false}
     ]},  
     {question: 'What is CSS?', answer: [
         {text: 'a', correct: false},
         {text: 'bDDD', correct: false},
-        {text: 'c', correct: false},
-        {text: 'dDDDD', correct: true}
+        {text: 'cdeee', correct: false},
+        {text: 'truea', correct: true}
     ]},  
-    {question: 'What SCRUM?', answer: [
-        {text: 'aDDD', correct: true},
-        {text: 'b', correct: false},
-        {text: 'cDDDD', correct: false},
-        {text: 'd', correct: false}
-    ]}
+    {question: 'What is a loop ?', answer: [
+        {text: 'a', correct: false},
+        {text: 'bDDD', correct: false},
+        {text: 'cdeee', correct: false},
+        {text: 'truea', correct: true}
+    ]}, 
         ]
     
 
